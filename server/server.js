a7010bce82cbdbaf08847db8b92b6869f1807fdf
Fábio Garcia Martins <fabiogarciamartins2019@gmail.com>
2025-06-09 13:58:41 -0300

    Adiçao do back end , loguin, cadastro e melhoramento no visual

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const jsonServer = require('json-server');
+const path = require('path');
+
+const app = express();
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const middlewares = jsonServer.defaults();
+
+// Servir arquivos estáticos da pasta public/
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// API fake do JSON Server em /api
+app.use('/api', middlewares, router);
+
+
+// Iniciar servidor
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando: http://localhost:${PORT}`);
+});
